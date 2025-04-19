9bb0d8ab766418bf8db2f1e7d4704db758c621e8
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-19 20:47:34 +0100

    Updated REST API Methods for Staff

--- CurveDentalManagement.API/src/main/java/com/curvedemtalapp/service/impl/StaffServiceImpl.java
+++ CurveDentalManagement.API/src/main/java/com/curvedemtalapp/service/impl/StaffServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class StaffServiceImpl implements StaffService {
@@ -23,4 +26,46 @@ public class StaffServiceImpl implements StaffService {
         return modelMapper.map(savedStaff, StaffDto.class);
     }
 
+    // REST API - Get Staff By ID
+    @Override
+    public StaffDto geStaffById(Long staffId) {
+        Staff staff = staffRepository.findAllById(staffId)
+                .orElseThrow(()->new RuntimeException("Staff doesn't exist with a given Id:" + staffId));
+        return modelMapper.map(staff, StaffDto.class);
+    }
+
+    // REST API - Get All Listed Staff
+    @Override
+    public List<StaffDto> getAllListedStaffs() {
+        List<Staff> staffs = staffRepository.findAll();
+        return staffs.stream().map((staff -> modelMapper.map(staff, StaffDto.class)))
+                .collect(Collectors.toList());
+    }
+
+    // REST API - Update Staff
+    @Override
+    public StaffDto updateStaff(Long staffId, StaffDto updateStaff) {
+        Staff staff = staffRepository.findAllById(staffId)
+                .orElseThrow(()-> new RuntimeException("Staff doesn't exist with a given Id:" + staffId));
+
+        staff.setFirstName(updateStaff.getFirstName());
+        staff.setLastName(updateStaff.getLastName());
+        staff.setStaffRole(updateStaff.getStaffRole());
+        staff.setEmail(updateStaff.getEmail());
+        staff.setGender(updateStaff.getGender());
+        staff.setPhoneNumber(updateStaff.getPhoneNumber());
+        staff.setAge(updateStaff.getAge());
+        staff.setAddress(updateStaff.getAddress());
+
+        Staff updateStaffObj = staffRepository.save(staff);
+        return modelMapper.map(updateStaffObj, StaffDto.class);
+    }
+
+    // REST API - Delete Staff
+    @Override
+    public void deleteStaff(Long staffId) {
+        Staff staff = staffRepository.findAllById(staffId)
+                .orElseThrow(()->new RuntimeException("Staff doesn't exist with given id:" + staffId));
+        staffRepository.deleteById(staffId);
+    }
 }
