3b9e7225d1aea58fa99a19d44e5f5f36c9dc594e
Louie Alfonso <lfriasalfonso@gmail.com>
2025-04-21 21:55:17 +0100

    Added UI Components & Services for Staffs

--- CurveDentalManagement.UI/src/components/staff/staff-lists.tsx
+++ CurveDentalManagement.UI/src/components/staff/staff-lists.tsx
@@ -1,6 +1,26 @@
+import { Link } from "react-router-dom";
+import { Toaster } from "@/components/ui/sonner"
+import { Button } from "@/components/ui/button";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { useGetAllStaffs } from "@/services/staff-services";
+
 const StaffLists = () => {
+
+  // Declare state variables
+  const { data, isLoading } = useGetAllStaffs();
+
+  // Handle loading state
+  if (isLoading) { return <div>Loading...</div>;}
+  if (!data) { return <div>No data found</div>;}
+
   return (
-    <div>StaffLists</div>
+    <div className="rounded-md border p-5 w-full overflow-x-auto">
+      <div className="flex justify-between items-center pb-5">
+          <Link to={`/staffs/add`}>
+            <Button className ="bg-green-500 hover:bg-green-600">Add Staff</Button>
+          </Link>
+      </div>
+    </div>  
   )
 }
 
--- CurveDentalManagement.UI/src/services/staff-services.ts
+++ CurveDentalManagement.UI/src/services/staff-services.ts
@@ -13,3 +13,74 @@ interface Staff {
     address?: string;
 }
 
+const API_BASE_URL = import.meta.env.VITE_BASE_URI_STAFFS;
+
+const staffServices = {
+
+    addNewStaff: async (newStaff: Staff) => {
+        const response = await axios.post(API_BASE_URL, newStaff);
+        return response.data;
+    },
+
+    getAllStaffs: async () => {
+        const response = await axios.get(API_BASE_URL);
+        return response.data;
+    },
+
+    getStaffById: async (id: string) => {
+        const response = await axios.get(`${API_BASE_URL}/${id}`);
+        return response.data;
+    },
+
+    updateCurrentStaff: async (currentStaff: Staff, id: string) => {
+        const response = await axios.put(`${API_BASE_URL}/${id}`, currentStaff);
+        return response.data;
+    },
+
+    deleteStaff: async (id: string) => {
+        await axios.delete(`${API_BASE_URL}/${id}`);
+    },
+
+}
+
+// React Query Hooks
+export const useAddNewStaff = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (newStaff: Staff) => staffServices.addNewStaff(newStaff),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['staffs'] });
+      },
+    });
+};
+
+export const useGetAllStaffs = () => {
+    return useQuery( 
+      { queryKey: ['staffs'], queryFn: staffServices.getAllStaffs });
+  };
+
+export const useGetStaffById = (id: string) => {
+    return useQuery(
+      { queryKey: ['staff', id], queryFn: () => staffServices.getStaffById(id) });
+  }
+export const useUpdateStaff = (id: string) => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (currentStaff: Staff) => staffServices.updateCurrentStaff(currentStaff, id),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['staff', id] });
+      },
+    });
+  };
+
+  export const useDeleteStaff = () => {
+    const queryClient = useQueryClient();
+    return useMutation({
+      mutationFn: (id: string) => staffServices.deleteStaff(id),
+      onSuccess: () => {
+        queryClient.invalidateQueries({ queryKey: ['staffs'] });
+      },
+    });
+  };
+
+export default staffServices
