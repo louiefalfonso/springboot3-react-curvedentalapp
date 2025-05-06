64cb41a8fed7bfe171c4368ef4dbe3db3815553f
Louie Alfonso <lfriasalfonso@gmail.com>
2025-05-06 22:17:54 +0100

    Update List & Details Components for Treatments

--- CurveDentalManagement.UI/src/components/treatments/list/list-treatment.tsx
+++ CurveDentalManagement.UI/src/components/treatments/list/list-treatment.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
-const TreatmentDetailsList = () => {
+const TreatmentDetailsList = ({treatmentData}:{treatmentData:any}) => {
   return (
-    <div>TreatmentDetailsList</div>
+    <>
+    <div className="rounded-md border p-5 w-full overflow-x-auto">
+      <h2 className="font-heading scroll-m-20 border-b pb-4 text-xl font-semibold tracking-tight first:mt-0">Doctor Information</h2>
+      <div className="min-w-full">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Treatment Code</TableHead>
+              <TableHead>Treatment Name</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow key={treatmentData.id}>
+            <TableCell>{treatmentData.treatmentCode}</TableCell>
+            <TableCell>{treatmentData.treatmentName}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>        
+      </div>
+    </div>
+    </>
   )
 }
 
