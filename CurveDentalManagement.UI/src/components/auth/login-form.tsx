7ac7cbddac46df6ab478e0846898d063e9aca571
Louie Alfonso <lfriasalfonso@gmail.com>
2025-07-22 19:52:14 +0100

    Update UI Components for List Treatments & Login

---
+++ CurveDentalManagement.UI/src/components/auth/login-form.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import axios from "axios";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom"
+import MainLogo from '@/assets/curve-dental-logo.jpeg';
+
+const LoginFormComponent = () => {
+
+  return (
+    <div>LoginFormComponent</div>
+  )
+}
+
+export default LoginFormComponent
