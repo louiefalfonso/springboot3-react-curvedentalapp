902a1db9aa5b95b6a296472aea20836e2b5357f4
Louie Alfonso <lfriasalfonso@gmail.com>
2025-08-28 20:29:14 +0100

    Update Application Propeties for JWT Token & Authentication

--- CurveDentalManagement.API/src/main/java/com/curvedemtalapp/repository/UserRepository.java
+++ CurveDentalManagement.API/src/main/java/com/curvedemtalapp/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.curvedemtalapp.repository;
 
 import com.curvedemtalapp.entity.User;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 }
