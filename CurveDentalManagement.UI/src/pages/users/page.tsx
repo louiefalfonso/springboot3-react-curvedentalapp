acb0476d444e9d2c68fbaa3c180e40f5db4dfc40
Louie Alfonso <lfriasalfonso@gmail.com>
2025-09-08 20:06:27 +0100

    Update Auth Services, User List Component & Page

--- CurveDentalManagement.UI/src/pages/users/page.tsx
+++ CurveDentalManagement.UI/src/pages/users/page.tsx
@@ -5,7 +5,7 @@ import MainLayout from "@/components/layout/layout"
 const UserListPage = () => {
   return (
     <MainLayout>
-      <Header Title ="Patient List"/>
+      <Header Title ="Users List"/>
         <div className="flex flex-1 flex-col gap-4 p-4">
           <div className="grid auto-rows-min gap-4 md:grid-cols-1">
             <ListAllUsers/>
