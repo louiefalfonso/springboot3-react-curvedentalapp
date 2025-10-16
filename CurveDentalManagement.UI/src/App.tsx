2e563a01123944a6692c5d2b69d927bd257d9035
Louie Alfonso <lfriasalfonso@gmail.com>
2025-10-16 20:32:56 +0100

    Update App Route & UI Components for List Doctors & Treatments

--- CurveDentalManagement.UI/src/App.tsx
+++ CurveDentalManagement.UI/src/App.tsx
@@ -20,6 +20,18 @@ import AddNewPatient from './components/patients/patient-add';
 import PatientDetails from './components/patients/patient-details';
 import UpdatePatient from './components/patients/patient-update';
 import UserListPage from './pages/users/page';
+import TreatmentDetails from './components/treatments/treatment-details';
+import UpdateTreatment from './components/treatments/treatment-update';
+import AddNewTreatment from './components/treatments/treatment-add';
+import AddNewStaff from './components/staffs/staff-add';
+import StaffDetails from './components/staffs/staff-details';
+import UpdateStaff from './components/staffs/staff-update';
+import AddNewBilling from './components/billings/billing-add';
+import BillingDetails from './components/billings/billing-details';
+import UpdateBilling from './components/billings/billiing-update';
+import AddNewAppointment from './components/appointments/appointment-add';
+import AppointmentDetails from './components/appointments/appointment-details';
+import UpdateAppointment from './components/appointments/appointment-update';
 
 function App() {
   const token = localStorage.getItem("token");
@@ -30,7 +42,7 @@ function App() {
         <Route path="/" element={<Navigate to={"/login"} replace/>} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={ <ProtectedRoute token={token}> <DashboardPage /> </ProtectedRoute> }/>
+        <Route path="/dashboard" element={ <ProtectedRoute token={token}><DashboardPage /></ProtectedRoute> }/>
        
         <Route path='/doctors' element={ <ProtectedRoute token={token}><DoctorListsPage/></ProtectedRoute> }/>
         <Route path='/doctors/add' element={ <ProtectedRoute token={token}><AddNewDoctor/></ProtectedRoute> }/>
@@ -43,9 +55,25 @@ function App() {
         <Route path='/patients/update/:id' element={ <ProtectedRoute token={token}><UpdatePatient/></ProtectedRoute> }/>
 
         <Route path='/staffs' element={ <ProtectedRoute token={token}><StaffListsPage/></ProtectedRoute> }/>
+        <Route path='/staffs/add' element={ <ProtectedRoute token={token}><AddNewStaff/></ProtectedRoute> }/>
+        <Route path='/staffs/details/:id' element={ <ProtectedRoute token={token}><StaffDetails/></ProtectedRoute> }/>
+        <Route path='/staffs/update/:id' element={ <ProtectedRoute token={token}><UpdateStaff/></ProtectedRoute> }/>
+
         <Route path='/treatments' element={ <ProtectedRoute token={token}><TreatmentListsPage/></ProtectedRoute> }/>
+        <Route path='/treatments/add' element={ <ProtectedRoute token={token}><AddNewTreatment/></ProtectedRoute> }/>
+        <Route path='/treatments/:id' element={ <ProtectedRoute token={token}><TreatmentListsPage/></ProtectedRoute> }/>
+        <Route path='/treatments/details/:id' element={ <ProtectedRoute token={token}><TreatmentDetails/></ProtectedRoute> }/>
+        <Route path='/treatments/update/:id' element={ <ProtectedRoute token={token}><UpdateTreatment/></ProtectedRoute> }/>
+      
         <Route path='/billings' element={ <ProtectedRoute token={token}><BillingsListPage/></ProtectedRoute> }/>
-        <Route path='/appointments' element={ <ProtectedRoute token={token}><AppointmentListsPage/></ProtectedRoute> }/>  
+        <Route path='/billings/aa' element={ <ProtectedRoute token={token}><AddNewBilling/></ProtectedRoute> }/>
+        <Route path='/billings/details/:id' element={ <ProtectedRoute token={token}><BillingDetails/></ProtectedRoute> }/>
+        <Route path='/billings/update/:id' element={ <ProtectedRoute token={token}><UpdateBilling/></ProtectedRoute> }/>
+
+        <Route path='/appointments' element={ <ProtectedRoute token={token}><AppointmentListsPage/></ProtectedRoute> }/>
+        <Route path='/appointments/add' element={ <ProtectedRoute token={token}><AddNewAppointment/></ProtectedRoute> }/> 
+        <Route path='/appointments/details/:id' element={ <ProtectedRoute token={token}><AppointmentDetails/></ProtectedRoute> }/>   
+        <Route path='/appointments/update/:id' element={ <ProtectedRoute token={token}><UpdateAppointment/></ProtectedRoute> }/>
 
         <Route path='/users' element={ <ProtectedRoute token={token}><UserListPage/></ProtectedRoute> }/> 
       </Routes>
--- CurveDentalManagement.UI/src/components/treatments/list/list-treatment.tsx
+++ CurveDentalManagement.UI/src/components/treatments/list/list-treatment.tsx
@@ -4,44 +4,91 @@ const TreatmentDetailsList = ({treatmentData}:{treatmentData:any}) => {
   return (
     <div className="rounded-md border p-5 w-full overflow-x-auto">
       <h2 className="font-heading scroll-m-20 border-b pb-4 text-xl font-semibold tracking-tight first:mt-0">Treatment Information</h2>
-      <div className="min-w-full">
+      <div className="min-w-full mt-3">
         <Table>
           <TableHeader>
             <TableRow>
-              <TableHead>Treatment Code</TableHead>
-              <TableHead>Treatment Description</TableHead>
               <TableHead>Treatment Name</TableHead>
+              <TableHead>Treatment Code</TableHead>
               <TableHead>Duration</TableHead>
               <TableHead>Cost</TableHead>
             </TableRow>
-          </TableHeader>
+          </ TableHeader>
           <TableBody>
             <TableRow key={treatmentData.id}>
-            <TableCell>{treatmentData.treatmentCode}</TableCell>
-            <TableCell>{treatmentData.description}</TableCell>
-            <TableCell>{treatmentData.treatmentName}</TableCell>
-            <TableCell>{treatmentData.duration}</TableCell>
-            <TableCell>{treatmentData.cost}</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
-              
+              <TableCell>{treatmentData.treatmentName}</TableCell>
+              <TableCell>{treatmentData.treatmentCode}</TableCell>
+              <TableCell>{treatmentData.duration}</TableCell>
+              <TableCell>{treatmentData.cost}</TableCell>
+            </TableRow>  
+          </TableBody>  
+        </Table>  
       </div>
       <div className="min-w-full mt-3">
         <Table>
           <TableHeader>
             <TableRow>
+              <TableHead>Treatment Description</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow key={treatmentData.id}>
+            <TableCell>{treatmentData.description}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table> 
+      </div>
+      <div className="min-w-full mt-3">
+       <Table>
+          <TableHeader>
+            <TableRow>
               <TableHead>Insurance Coverage</TableHead>
-              <TableHead>Follow Up Care</TableHead>
-              <TableHead>Risk & Benefits</TableHead>
-              <TableHead>Indications</TableHead>
             </TableRow>
           </TableHeader> 
           <TableBody>
             <TableRow key={treatmentData.id}>
               <TableCell>{treatmentData.insuranceCoverage}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>   
+      </div>
+      <div className="min-w-full mt-3">
+       <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Follow Up Care</TableHead>
+            </TableRow>
+          </TableHeader> 
+          <TableBody>
+            <TableRow key={treatmentData.id}>
               <TableCell>{treatmentData.followUpCare}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>   
+      </div>
+      <div className="min-w-full mt-3">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Risk & Benefits</TableHead>
+            </TableRow>
+          </TableHeader> 
+          <TableBody>
+            <TableRow key={treatmentData.id}>
               <TableCell>{treatmentData.riskBenefits}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>  
+      </div>
+      <div className="min-w-full mt-3">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Indications</TableHead>
+            </TableRow>
+          </TableHeader> 
+          <TableBody>
+            <TableRow key={treatmentData.id}>
               <TableCell>{treatmentData.indications}</TableCell>
             </TableRow>
           </TableBody>
